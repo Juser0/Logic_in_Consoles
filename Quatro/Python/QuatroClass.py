0f66b96ab343185da89bbbbf26374835bbba4715
Ju <justuser0129@gmail.com>
2022-07-13 22:28:39 +0900

    gitignore + module separating in python

---
+++ Quatro/Python/QuatroClass.py
@@ -0,0 +1,36 @@
+'''
+카드 클래스
+카드는 빨강, 파랑, 노랑, 초록, 검정 카드로 이루어져있다.
+검정색 카드는 0카드 2개만 존재하며, 나머지 4가지 색의 카드는 1~6까지 존재한다
+생성자를 통해 색, 숫자를 입력받아 객체를 생성한다
+'''
+
+class Card:
+    def __init__(self, color, num):
+        self.color = color
+        self.num = num
+        self.visible = False
+
+'''
+가상 플레이어 클래스
+가상 플레이어는 플레이어들과 교환을 진행하는 객체이다.
+두 플레이어와의 교환을 각각 1번씩 해야하므로 두 플레이어에 대한 교환 여부를 boolean 자료형으로 받는다.
+그리고 교환할 카드가 담길 리스트를 만들어둔다.
+'''
+
+class Vplayer:
+    hands = []
+    def __init__(self):
+        self.is_traded0 = False
+        self.is_traded1 = False
+
+'''
+플레이어 클래스
+플레이어는 최대 4장의 카드를 들고 있을 수 있는 핸드 리스트와, 카드가 오픈된 리스트를 가진다.
+또한, 처음 손패가 안 좋을때 새로 교환할 수 있는 멀리건 횟수를 생성할 때 2회로 초기화해준다.
+'''
+class Player:
+    hands = []
+    opened = []
+    def __init__(self):
+        self.mulligan = 2
---
+++ Quatro/Python/QuatroFunc.py
@@ -0,0 +1,39 @@
+import random
+import os
+
+def print_Deck(list_name):
+    for i in range(len(list_name)):
+        print("Color : " + list_name[i].color + " / Num : " + str(list_name[i].num))
+
+def print_Players(list_name):
+    for i in range(len(list_name)):
+        print(str(i + 1) + "번", end = '   ')
+        for j in range(3):
+            if list_name[i].hands[j].visible == True:
+                print(str(list_name[i].hands[j].color) + ' ' + str(list_name[i].hands[j].num) + ' ', end = '  ')
+            else:
+                print('X ', end = '  ')
+
+def trade(wp, to, gived):
+    take = Card('Black', 0)
+    for i in range(len(players[wp].opened)):
+        for j in range(3):
+            if Vplayers[to - 1].hands[j].num == 0:
+                Vplayers[to - 1].hands[j] = gived
+                Vplayers[to - 1].hands[j].visible = True
+                return take
+            elif players[wp].opened.find(Vplayers[to - 1].hands[j].num) == -1 and players[wp].opened.find(Vplayers[to - 1].hands[j].color) == -1:
+                take = Vplayers[to - 1].hands[j]
+            else:
+                if take.num < Vplayers[to - 1].hands[j].num:
+                    take = Vplayers[to - 1].hands[j]
+                elif take.num == Vplayers[to - 1].hands[j].num:
+                    if len(take.color) == 5 and len(Vplayers[to - 1].hands[j].color) == 6:
+                        take = Vplayers[to - 1].hands[j]
+                    elif len(take.color) > len(Vplayers[to - 1].hands[j].color) and len(Vplayers[to - 1].hands[j].color) != 5:
+                        take = Vplayers[to - 1].hands[j]
+    idx = Vplayers[to - 1].hands.index(take)
+    Vplayers[to - 1].hands[idx] = gived
+    return take
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
